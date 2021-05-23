5d39f69c86ce79044a25b724532a093229b6dd68
trdp30 <trdp30@gmail.com>
2021-05-23 11:02:28 +0530

    pagination added

--- src/components/skill-helpers/skill-list-view.js
+++ src/components/skill-helpers/skill-list-view.js
@@ -1,12 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useMemo, useState } from "react";
 import SkillCard from "./skill-card";
+import { chunk } from "../../utils/array";
+import { Pagination } from "semantic-ui-react";
 
 function SkillListView(props) {
   const { skills, updateSelectedSkills, selectedSkills } = props;
+  const collections = useMemo(() => {
+    if (skills.length) {
+      return chunk(skills, 10);
+    }
+    return [];
+  }, [skills]);
+  const [pageIndex, updatePageIndex] = useState(0);
+
+  const handlePaginationChange = (e, { activePage }) => {
+    updatePageIndex(() => activePage - 1);
+  };
 
   return (
     <Fragment>
-      {skills.map((skill) => (
+      {collections[pageIndex].map((skill) => (
         <SkillCard
           key={skill.id}
           skill={skill}
@@ -14,6 +27,15 @@ function SkillListView(props) {
           selectedSkills={selectedSkills}
         />
       ))}
+      <Pagination
+        boundaryRange={0}
+        ellipsisItem={null}
+        firstItem={null}
+        lastItem={null}
+        totalPages={collections.length}
+        activePage={pageIndex + 1}
+        onPageChange={handlePaginationChange}
+      />
     </Fragment>
   );
 }
